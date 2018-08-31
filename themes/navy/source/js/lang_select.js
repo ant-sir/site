ce63b7940d974c1914b427b6771549d273ec9734
朱艳雷10194116 <zhu.yanlei@zte.com.cn>
2018-08-31 14:24:21 +0800

    合并主干

--- themes/navy/source/js/lang_select.js
+++ themes/navy/source/js/lang_select.js
@@ -1,15 +1,18 @@
-(function(){
+(function() {
   'use strict';
 
-  function changeLang(){
+  var Cookies = window.Cookies.noConflict();
+
+  function changeLang() {
     var lang = this.value;
     var canonical = this.dataset.canonical;
     if (lang === 'zh-cn') lang = '';
     if (lang) lang += '/';
 
+	Cookies.set('nf_lang', lang, { expires: 365 });
     location.href = '/' + lang + canonical;
   }
 
   document.getElementById('lang-select').addEventListener('change', changeLang);
   document.getElementById('mobile-lang-select').addEventListener('change', changeLang);
-})();
+}());
