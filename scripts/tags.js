d578f689d3ed15ba0e534f2336d24813d38a4b30
朱艳雷10194116 <zhu.yanlei@zte.com.cn>
2019-03-19 15:31:00 +0800

    一个基础的gitlog tag

--- scripts/tags.js
+++ scripts/tags.js
@@ -2,6 +2,8 @@
 
 'use strict';
 
+var process = require('child_process');
+
 hexo.extend.tag.register('note', function(args, content) {
   var className = args.shift();
   var header = '';
@@ -19,42 +21,93 @@ hexo.extend.tag.register('note', function(args, content) {
 }, true);
 
 
-hexo.extend.tag.register('github', function(args) {
-  // var className = args.shift();
+hexo.extend.tag.register('gitlog', function(args) {
   var badge = [];
   var badge_end = [];
   var header = [];
   var info = [];
+  var log_start = [];
   var log = [];
-  var result = '<div id="badge-container-ant-sir-site-213329" class="hexo-github" style="width: 100%"><div class="github-badge"><div class="header"><div class="icon">  <span class="mega-octicon octicon-mark-github"></span>  <div class="avatar"><img src="https://avatars3.githubusercontent.com/u/7426281?v=4"></div></div><div class="content">  <h3 class="user"><a href="https://github.com/ant-sir" target="_blank" class="user-link">ant-sir</a></h3>  <h3 class="repo"><a href="https://github.com/ant-sir/site" target="_blank" class="repo-link">site</a></h3></div></div><div class="info"><div class="icon">  <div class="circle"></div>  <span class="octicon octicon-history"></span></div><div class="status"><a href="https://github.com/ant-sir/site/commit/213329eb2c7e171e63b463ef22ed4df421f2764e" target="_blank" class="commit-link" title="修改“关于”页面">213329</a>  <span>, 1 commits behind</span></div></div><div class="commits"><div class="line"></div><div class="entry"><div class="item commit-before">  <div class="icon">    <div class="circle"></div>    <span class="octicon octicon-code"></span>  </div>  <div class="text"><span class="commits-count-before">98</span> commits</div></div><div class="item commit-current">  <div class="icon">    <div class="circle active"></div>    <span class="octicon octicon-pencil"></span>  </div>  <div class="text active"><a href="https://github.com/ant-sir/site/commit/213329eb2c7e171e63b463ef22ed4df421f2764e" target="_blank" class="commit-link" title="修改“关于”页面">213329</a>, referenced in this article</div></div><div class="item commit-after" style="display: none;">  <div class="icon">    <div class="circle"></div>    <span class="octicon octicon-code"></span>  </div>  <div class="text"><span class="commits-count-after">0</span> commits</div></div><div class="item commit-latest" style="display: block;">  <div class="icon">    <div class="circle"></div>    <span class="octicon octicon-clock"></span>  </div>  <div class="text"><a href="https://github.com/ant-sir/site/commit/b98e9ae4826482e9d2ddac5a3215fe13527dc7c3" target="_blank" class="commit-link" title="修改分类主页">b98e9a</a>, latest</div></div><div class="item commit-up-to-date" style="display: none;">  <div class="icon">    <div class="circle"></div>    <span class="octicon octicon-check"></span>  </div>  <div class="text">Up-to-date</div></div></div></div></div></div>';
+  var log_end = [];
+  var items = [];
+  var item = [];
+  var result = '';
+  
+  var user = args[0];
+  var repo = args[1];
+  var format = args[2];
+  var autoExpand = args[3];
+
+  var stdout = process.execSync('git log ' + format, function(err, stdout, stderr) {
+    if (err) {
+      console.log(stderr);
+    } else {
+      // console.log(stdout.trim().split('\n'));
+    }
+  });
+
+  items = stdout.toString().trim().split('\n');
 
   badge = ['<div id="badge-container" class="hexo-github" style="width: 100%">',
-            '<div class="github-badge">'];
+            '<div class="github-badge">'
+  ];
+
   header = [  '<div class="header">',
                 '<div class="icon"> <span class="mega-octicon octicon-mark-github"></span>',
                   '<div class="avatar">',
-                    '<img src="https://avatars3.githubusercontent.com/u/7426281?v=4">',
+                    '<img src="https://avatars3.githubusercontent.com/' + user + '?v=4">',
                   '</div>',
                 '</div>',
                 '<div class="content">',
-                  '<h3 class="user"><a href="https://github.com/ant-sir" target="_blank" class="user-link">ant-sir</a></h3>',
-                  '<h3 class="repo"><a href="https://github.com/ant-sir/site" target="_blank" class="repo-link">site</a></h3>',
+                  '<h3 class="user"><a href="https://github.com/' + user + '" target="_blank" class="user-link">'+ user + '</a></h3>',
+                  '<h3 class="repo"><a href="https://github.com/' + user + '/' + repo + '" target="_blank" class="repo-link">' + repo + '</a></h3>',
                 '</div>',
-              '</div>'];
+              '</div>'
+  ];
+
+  console.log(items);
+  item = items.shift().split(' ');
+
   info = [    '<div class="info">',
                 '<div class="icon">',
                   '<div class="circle"></div> <span class="octicon octicon-history"></span>',
                 '</div>',
                 '<div class="status">',
-                  '<a href="https://github.com/ant-sir/site/commit/213329eb2c7e171e63b463ef22ed4df421f2764e" target="_blank" class="commit-link" title="修改“关于”页面">213329</a> <span>, 1 commits behind</span>',
+                  '<span>' + item[0] + ' </span><a href="https://github.com/' + user + '/' + repo + '/commit/' + item[1] + '" target="_blank" class="commit-link" title="' + item[2] + '">' + item[1] + '</a> <span>: ' + item[2] + '</span>',
                 '</div>',
-              '</div>'];
-  log = [];
+              '</div>'
+  ];
 
+  log_start = [
+              '<div class="commits">',
+                '<div class="line"></div>',
+                  '<div class="entry">'
+  ];
 
+  items.forEach(element => {
+    item = element.split(' ');
+    log = log.concat([
+                    '<div class="item commit-before">',
+                      '<div class="icon">',
+                          '<div class="circle"></div> <span class="octicon octicon-code"></span>',
+                      '</div>',
+                      '<div class="text">',
+                        '<span>' + item[0] + ' </span><a href="https://github.com/' + user + '/' + repo + '/commit/' + item[1] + '" target="_blank" class="commit-link" title="' + item[2] + '">' + item[1] + '</a> <span>: ' + item[2] + '</span>',
+                    '</div>']);
+  });
 
-  badge_end = [   '</div>',
-                '</div>'];
+  log_end = [
+                  '</div>',
+                '</div>',
+              '</div>'
+  ];
 
+  badge_end = [
+            '</div>',
+          '</div>'
+  ];
+
+  result = badge.join('') + header.join('') + info.join('') + log_start.join('') + log.join('') + log_end.join('') + badge_end.join('');
+  
   return result;
 }, true);
--- source/categories/index.md
+++ source/categories/index.md
@@ -9,6 +9,6 @@ title: 欢迎
 本站主要记录一些学习笔记与经验爱好。由于水平有限可能并不是所有内容都很准确。如果你有不同见解可以在下面评论区留言，也可发邮件交流。
 
 ## 修改历史
-以下内容使用`git log -20 --pretty="%cd %s" --date=short`得到。
-{% github ant-sir site 213329 true %}
-{% endgithub %}
+以下内容使用`git log -20 --pretty="%cd %h %s" --date=short`得到。
+{% gitlog ant-sir site '-20 --pretty="%cd %h %s" --date=short' true %}
+{% endgitlog %}
