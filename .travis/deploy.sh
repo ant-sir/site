537a69fcc30fe14ad2347878c5fdd557ca7c9c94
zhu.yanlei <zhuyanleism@sina.com>
2019-03-23 10:54:59 +0800

    添加自动更新docsearch
    Change-Id: Iad2215908b96832dd0cdeff7dca38bdd08ca6ba5

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Decrypt the private key
 openssl aes-256-cbc -K $encrypted_06b8e90ac19b_key -iv $encrypted_06b8e90ac19b_iv -in .travis/ssh_key.enc -out ~/.ssh/id_rsa -d
-echo $encrypted_06b8e90ac19b_key $encrypted_06b8e90ac19b_iv
+#echo $encrypted_06b8e90ac19b_key $encrypted_06b8e90ac19b_iv
 chmod 600 ~/.ssh/id_rsa
 # Start SSH agent
 eval $(ssh-agent)
@@ -16,3 +16,19 @@ git config --global user.email zhuyanleigm@gmail.com
 git clone git@github.com:ant-sir/ant-sir.github.io.git .deploy_git
 # Deploy to GitHub
 npm run deploy
+
+
+#doc search build
+cd ~
+git clone https://github.com/algolia/docsearch-scraper.git
+git clone git@github.com:ant-sir/site-search-config.git
+cd docsearch-scraper/
+pipenv install
+pipenv shell
+export APPLICATION_ID=IKDE0Z5WJI
+export API_KEY=79c95c636b06d0d69e8882863b9355ac
+./docsearch run ~/site-search-config/config.json
+
+cd ~/site-search-config
+git commit -m "update config" config.json
+git push origin master
